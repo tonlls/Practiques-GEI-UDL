a0b454c57a76b01ea33130e4be723086621de342
tonlls <tonlls1999@gmail.com>
2019-11-28 10:19:47 +0100

    practica 2 programacio started

--- Programacio_1/Practica_2/ex1.cpp
+++ Programacio_1/Practica_2/ex1.cpp
@@ -1,10 +1,31 @@
 #include <string.h>
 #include <stdio.h>
+#include <ctype.h>
 #define MAX_WORDS 10
 #define MAX_CHARS 50
+#define END_TEXT_CHAR '.'
+void str_toupper(char str[MAX_CHARS]){
+	for(int i=0;i<strlen(str);i++){
+		str[i]=toupper(str[i]);
+	}
+}
+int strcompare(char str1[MAX_CHARS],char str2[MAX_CHARS]){
+	char copy1[MAX_CHARS],copy2[MAX_CHARS];
+	strcpy(copy1,str1);
+	strcpy(copy2,str2);
+	str_toupper(copy1);
+	str_toupper(copy2);
+	return strcmp(copy1,copy2);
+}
+int is_letter(char c){
+	if((c>='a'&&c<='z')||(c>='A'&&c<='Z')){
+		return 1;
+	}
+	return 0;
+}
 int find_char(char c,char str[MAX_CHARS]){
 	for(int i=0;i<strlen(str);i++){
-		if(str[i]==c){
+		if(toupper(str[i])==toupper(c)){
 			return i;
 		}
 	}
@@ -15,44 +36,94 @@ void compact_string(char str[MAX_CHARS],int start_pos){
 		str[i]=str[i+1];
 	}
 }
-int is_anagram(char word[MAX_CHARS],char str[MAX_CHARS]){
+//return 1 if is anagram and 0 if is not an anagram
+int is_anagram(char anagram[MAX_CHARS],char str[MAX_CHARS]){
+	char copy[MAX_CHARS]="";
 	char temp[MAX_CHARS]="";
-	int pos;
-	if(strlen(word)!=strlen(str)){
+	int pos,len;
+	strcpy(copy,str);
+	//printf("(%s)",copy);
+	if(strlen(anagram)!=strlen(copy)){
 		return 0;
 	}
-	for(int i=0;i<strlen(word);i++){
-		pos=find_char(word[i],str);
+	for(int i=0;i<strlen(anagram);i++){
+		pos=find_char(anagram[i],copy);
 		if(pos==-1){
 			return 0;
 		}
-		temp[i]=str[pos];
-		compact_string(str,pos);
+		temp[i]=copy[pos];
+		compact_string(copy,pos);
 	}
+	if(strcompare(temp,anagram)==0){
+		return 1;
+	}
+	return 0;
 }
 //returns -1 if is not an anagram and the position of the searching word if it is
 int find_anagram(int n_words,char words[MAX_WORDS][MAX_CHARS],char str[MAX_CHARS]){
 	for(int i=0;i<n_words;i++){
-		//for
+		if(is_anagram(words[i],str)==1){
+			return i;
+		}
 	}
+	return -1;
 }
-void get_searching_words(char words[MAX_WORDS][MAX_CHARS]){
+//get chars from stdin ended by end char
+int get_words(char words[MAX_WORDS][MAX_CHARS],char end_char){
 	char c;
 	char str[MAX_CHARS]="";
 	int i=0,j=0;
-	scanf("%c",&c);
-	while(c!='.'){
-		i=0;
-		while(c!=' '){
+	do{
+		scanf("%c",&c);
+		while(is_letter(c)){
 			str[i]=c;
 			i++;
+			scanf("%c",&c);
 		}
 		str[i]='\0';
-		strcpy(words[j],str);
-	}
+		if(i>=1){
+			strcpy(words[j],str);
+			str[0]='\0';
+			j++;
+			i=0;
+		}
+	}while(c!=end_char);
+	return j;
 }
 
+int get_next_word(char end,char str[MAX_CHARS]){
+	char c;
+	int i=0;
+	scanf("%c",&c);
+	while(is_letter(c)){
+		str[i]=c;
+		i++;
+		scanf("%c",&c);
+	}
+	str[i]='\0';
+	if(c==end){
+		return 1;
+	}
+	return 0;
+}
 int main(){
-	char words[MAX_WORDS][MAX_CHARS];
-	char word[MAX_CHARS];
+	int n_search_words,end,index;
+	int n_found[MAX_WORDS]={0};
+	char search_words[MAX_WORDS][MAX_CHARS];
+	char actual_word[MAX_CHARS];
+	n_search_words=get_words(search_words,END_TEXT_CHAR);
+	printf("Found Anagrams:\n");
+	end=get_next_word(END_TEXT_CHAR,actual_word);
+	while(!end){
+		index=find_anagram(n_search_words,search_words,actual_word);
+		if(index!=-1){
+			n_found[index]++;
+			printf("%s(%s), ",actual_word,search_words[index]);
+		}
+		end=get_next_word(END_TEXT_CHAR,actual_word);
+	}
+	printf("\nNum of found anagrams:\n");
+	for(int i=0;i<n_search_words;i++){
+		printf("%s -> %i\n",search_words[i],n_found[i]);
+	}
 }
--- Programacio_1/Practica_2/ex2.cpp
+++ Programacio_1/Practica_2/ex2.cpp
@@ -1,1 +1,99 @@
-int main () {}
+#include <stdio.h>
+#include <string.h>
+
+#define FREE_PLACE 'O'
+#define DISP_FREE_PLACE ' '
+
+#define WALL 'X'
+#define DISP_WALL 'X'
+
+#define STEP 'i'
+#define DISP_STEP 'i'
+
+#define START_POS 'I'
+#define DISP_START_POS 'I'
+
+#define ROWS 10
+#define COLUMNS 20
+
+#define NORTH 'n'
+#define SOUTH 's'
+#define EAST 'e'
+#define WEST 'w'
+
+char get_display_char(char c){
+	switch(c){
+		case FREE_PLACE:return DISP_FREE_PLACE;
+		case WALL:return DISP_WALL;
+		case STEP:return DISP_STEP;
+		case START_POS:return DISP_START_POS;
+	}
+}
+
+void display_laberinth(char laberinth[ROWS][COLUMNS]){
+	for(int i=ROWS-1;i<=0;i--){
+		for(int j=COLUMNS-1;j<=0;j--){
+			printf("")
+		}
+	}
+}
+
+void walk(int *x,int *y,char c){
+	switch(c){
+		case NORTH:{
+			y++;
+		}break;
+		case SOUTH:{
+			y--;
+		}break;
+		case EAST:{
+			x++;
+		}break;
+		case WEST:{
+			x--;
+		}break;
+	}
+}
+
+int get_directions(char directions[]){
+	scanf("%s",directions);
+	for(int i=0;i<strlen(directions);i++){
+		if(directions[i]!=NORTH&&directions[i]!=SOUTH&&directions[i]!=EAST&&directions[i]!=WEST){
+			return 0;
+		}
+	}
+	return 1;
+}
+int check_laberinth_row(char row[COLUMNS]){
+	if(strlen(row)!=COLUMNS ){
+		return 0;
+	}
+	for(int i=0;i<COLUMNS;i++){
+		if(row[i]!=FREE_PLACE && row[i]!=WALL){
+			return 0;
+		}
+	}
+	return 1;
+}
+/*
+gets the laberinth from stdin and returns 0 if the format isn't correct and 1 if is correct
+*/
+int get_laberinth(char laberinth[ROWS][COLUMNS]){
+	for(int i=0;i<ROWS;i++){
+		scanf("%s",laberinth[i]);
+		if(!check_laberinth_row(laberinth[i])){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int get_start_pos(int *x,int *y){
+	scanf("%i",x);
+}
+
+void draw_path(char laberinth[ROWS][COLUMNS])
+int main () {
+	char laberinth[ROWS][COLUMNS+]={""};
+
+}
