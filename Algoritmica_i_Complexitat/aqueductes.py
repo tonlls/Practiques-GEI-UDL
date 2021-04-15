73e1c1af9e2fe3d316548949e5461e863f0e323e
tonlls <tonlls1999@gmail.com>
2021-04-15 15:43:38 +0200

    aqueducte

--- Algoritmica_i_Complexitat/aqueductes.py
+++
@@ -1,9 +0,0 @@
-def read_file(filename):
-	lines=None
-	rects=[]
-	with f=open(filename,'r'):
-		lines=f.readlines()
-	n,h,a,b=lines[0].split(' ')
-	for l in lines[1:]:
-		rects.append(l.split(' ')
-	return n,h,a,b,rects
